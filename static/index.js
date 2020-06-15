e85101589e3f2cffad9128605fef55d3ff8c7d00
Andres Rios <rianby64@gmail.com>
2020-06-15 21:32:20 +0300

    Improve the way to edit the route

--- static/index.js
+++ static/index.js
@@ -9,11 +9,14 @@ async function init() {
         searchControlProvider: 'yandex#search'
     });
 
+    window.myMap = myMap;
+
     const routesList = document.querySelector('#routes-list');
     const routeInput = document.querySelector('#route-input');
     const routeSelectedLabel = document.querySelector('#route-selected-title');
     const routeEditBtn = document.querySelector('#route-edit');
-    const routeFinishBtn = document.querySelector('#route-finish');
+    const routeEditFinishBtn = document.querySelector('#route-edit-finish');
+    const routeEditCancelBtn = document.querySelector('#route-edit-cancel');
     const routes = await fetch('/api/routes').then(routes => routes.json())
 
     routes.forEach(route => {
@@ -23,14 +26,30 @@ async function init() {
         routesList.appendChild(option);
     });
 
+    let listener1 = null;
+    let listener2 = null;
+    let listener3 = null;
+
     routeInput.addEventListener('change', (e) => {
         const id = e.target.value;
         const route = routes.find(route => route.id === id);
+
+        routeEditFinishBtn.hidden = true;
+        routeEditBtn.hidden = true;
+        routeEditCancelBtn.hidden = true;
+        routeSelectedLabel.textContent = '-';
+
         if (!route) {
             return;
         }
 
+        myMap.geoObjects.removeAll();
+        listener1 && routeEditBtn.removeEventListener('click', listener1);
+        listener2 && routeEditFinishBtn.removeEventListener('click', listener2);
+        listener3 && routeEditCancelBtn.removeEventListener('click', listener3);
+        routeEditBtn.hidden = false;
         routeSelectedLabel.textContent = route.title;
+
         // Creating a polyline.
         const myPolyline = new ymaps.Polyline(
             // Specifying the coordinates of the vertices.
@@ -64,12 +83,14 @@ async function init() {
             });
         });
 
-        routeEditBtn.addEventListener('click', e => {
+        listener1 = e => {
             myPolyline.editor.startEditing();
-        });
-
-        routeFinishBtn.addEventListener('click', e => {
-            myPolyline.editor.stopEditing();
+            routeEditBtn.hidden = true;
+            routeEditFinishBtn.hidden = false;
+            routeEditCancelBtn.hidden = false;
+        };
+        listener2 = e => {
+            listener3(e);
             fetch(`/api/routes/${route.id}/lines`, {
                 method: 'put',
                 body: JSON.stringify(route.lines),
@@ -77,6 +98,15 @@ async function init() {
                     'Content-Type': 'application/json'
                 }
             });
-        })
+        };
+        listener3 = e => {
+            myPolyline.editor.stopEditing();
+            routeEditBtn.hidden = false;
+            routeEditFinishBtn.hidden = true;
+            routeEditCancelBtn.hidden = true;
+        }
+        routeEditBtn.addEventListener('click', listener1);
+        routeEditFinishBtn.addEventListener('click', listener2);
+        routeEditCancelBtn.addEventListener('click', listener3);
     });
 }
